befdf011b7a5f32873c5921ca894f1e8fdd56c93
Tristan McGrath <tristanmcgrath22@gmail.com>
2017-12-09 01:04:38 -0700

    add: Add file comments for index.html, app.js, app.css

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-var tryCount = 0;
+var tryCount = 0;    //This is a global variable responsible for keeping track of how many guesses the player has made throughout the game.
 
-$(document).ready(function() {
-    var randomArray = shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+$(document).ready(function() {                               //This is the function responsible for creating a new shuffled set of cards on load
+    var randomArray = shuffle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);  //This is the array that will be randomized based upon the below shuffle function
 
-    for (var i=0; i<16; i++) {
-        var card = $('li').eq(i);
+    for (var i=0; i<16; i++) {       //Cylcles through 16 times for the 16 cards to be generated
+        var card = $('li').eq(i);    //Each iteration sets variable card equal to the i-th li card element
 
-        if (randomArray[i] === 1 || randomArray[i] === 2) {
-          card.append( "<i class=\"fa fa-anchor\"></i>" );
-        } else if (randomArray[i] === 3 || randomArray[i] === 4) {
-          card.append( "<i class=\"fa fa-diamond\"></i>" );
+        if (randomArray[i] === 1 || randomArray[i] === 2) {           //Since the randomArray will randomly have one of each number 1 through 16, the
+          card.append( "<i class=\"fa fa-anchor\"></i>" );            //the following logic is set up to append the appropriate class code for a given
+        } else if (randomArray[i] === 3 || randomArray[i] === 4) {    //fa icon to the <i> element. This for-loop will result in 16 cards with 8 different
+          card.append( "<i class=\"fa fa-diamond\"></i>" );           //icons and two of each icon.
         } else if (randomArray[i] === 5 || randomArray[i] === 6) {
           card.append( "<i class=\"fa fa-rebel\"></i>" );
         } else if (randomArray[i] === 7 || randomArray[i] === 8) {
@@ -28,7 +28,7 @@ $(document).ready(function() {
 });
 
 
-function shuffle(array) {
+function shuffle(array) {                                          //This is the array shuffle function which was provided to us by Udacity.
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
@@ -42,14 +42,14 @@ function shuffle(array) {
 }
 
 
-function matchTest(){
-    var first = $('.selected').find('i').eq(0).attr("class");  //Class names for the fa icons are what is used
-    var second = $('.selected').find('i').eq(1).attr("class"); //to test for matches.
-    tryCount += 1;                                             //Every two card flips will tricker an increment of guess count
-    $('#guess-counter').html(`Guess Count: ${tryCount}`);      //Visually updates the guess count for the player
+function matchTest(){    //Fucntion to test for matches and add/remove styles accordingly for each scenario
+    var first = $('.selected').find('i').eq(0).attr("class");  //Class names for the fa icons are what is used to test for matches
+    var second = $('.selected').find('i').eq(1).attr("class");
+    tryCount += 1;                                             //Every two card flips will trigger an increment of guess counter
+    $('#guess-counter').html(`Guess Count: ${tryCount}`);      //Visually updates the guess count for the player at top of screen
 
-    if (tryCount > 12 && tryCount < 16) {          //This block of code progressively decreases the number
-      $('#star-count').html(`☆☆☆☆`);               //of stars with the more guesses it takes to solve
+    if (tryCount > 12 && tryCount < 16) {          //This block of code progressively decreases the number of stars with the more
+      $('#star-count').html(`☆☆☆☆`);               //guesses it takes to solve. Fewer guesses will leave more stars.
     } else if (tryCount >= 16 && tryCount < 20) {
       $('#star-count').html(`☆☆☆`);
     } else if (tryCount >= 20 && tryCount < 23) {
@@ -58,9 +58,9 @@ function matchTest(){
       $('#star-count').html(`☆`);
     }
 
-    if ( first === second ) {                           //If cards match...
-        $('.selected').toggleClass("match", true);      //apply the .match class which changes the background color.
-        $('.selected').toggleClass("selected", false);  //remove the .selected class which is for cards currently being guessed.
+    if ( first === second ) {                           //If cards match according to the class names...
+        $('.selected').toggleClass("match", true);      //apply the .match class which changes the card background color.
+        $('.selected').toggleClass("selected", false);  //remove the .selected class which is for live cards currently being guessed.
     } else {
         setTimeout(function() {
             $('.selected').toggleClass("selected", false); //If the cards do not match, flip them back over after half a second
@@ -71,7 +71,8 @@ function matchTest(){
         setTimeout(function() {
             var starCount = $('#star-count').html();        //stores the star counts html in a variable for victory pop-up message
 
-            $('.winner-overlay').css({"display": "inline-block", "position": "absolute", "z-index": "1", "top": "0", "left": "0"}); //creates grey overlay
+            $('.winner-overlay').css({"display": "inline-block", "position": "absolute",
+                                      "z-index": "1", "top": "0", "left": "0"}); //makes grey page overlay appear at game finish
 
             var confettiSettings = { target: 'my-canvas', height: 1100 };  //this code makes use of the confetti code stored in js/index.min.js
             var confetti = new ConfettiGenerator(confettiSettings);   //confetti will display on player victory
@@ -86,12 +87,13 @@ function matchTest(){
 }
 
 
-$('.card').click(function(evt){
-    var currentCardClass = $(evt.target).attr("class");
-    if ($('.selected').length < 2 && (currentCardClass !== "card selected" && currentCardClass[0] !== "f")) {
-        $(evt.target).toggleClass("selected");
+$('.card').click(function(evt){                                 //This is the card click event function
+    var currentCardClass = $(evt.target).attr("class");         //Class attribute used to test if valid card has been clicked (i.e. not already chosen)
+    if ($('.selected').length < 2 && (currentCardClass !== "card selected" && currentCardClass !== "card match" && currentCardClass[0] !== "f")) {
+                                              //Above if statement ensures only 2 cards can be selected at a time, clicking cards already flipped won't work
+        $(evt.target).toggleClass("selected");                  //Add the class .selected which will apply appropriate stylings
         if ($('.selected').length === 2) {
-            matchTest();
+            matchTest();                                //If the click flips over a second card, then matchTest() is invoked
         }
     }
 });
